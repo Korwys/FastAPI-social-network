d8b2865685a4fa096f25f96ae301a339e03130ba
Daniil S <sidorenkodk@gmail.com>
2023-01-17 15:09:55 +0300

    add: redis cache. post caching when fetch from db

--- api/posts.py
+++ api/posts.py
@@ -2,14 +2,15 @@ import fastapi
 from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
 from starlette import status
+from starlette.responses import JSONResponse
 
 from db.config import get_db
 from services.auth import get_current_user
 
-from db.crud import add_new_post_in_db, fetch_all_posts_from_db, fetch_one_post, update_post, remove_post_from_db, \
-    check_post_author, change_likes_or_dislikes
-from db.models import Post, Likes, Dislikes
+from db.crud import add_new_post_in_db, update_post, remove_post_from_db, check_post_author
+from db.models import Post
 from db.schemas import PostInDB, PostCreate, UserInDB, PostUpdate
+from services.cache import fetch_post_from_cache, fetch_posts_from_cache, change_count_of_users_emotions
 
 router = fastapi.APIRouter()
 
@@ -22,38 +23,39 @@ def create_new_post(post: PostCreate, db: Session = Depends(get_db),
 
 @router.get('/', response_model=list[PostInDB], status_code=status.HTTP_200_OK)
 def get_all_posts(db: Session = Depends(get_db), skip: int = 0, limit: int = 100) -> list[Post]:
-    return fetch_all_posts_from_db(db, skip, limit)
+    return fetch_posts_from_cache(quantity=limit, db=db)
 
 
 @router.get('/{post_id}', response_model=PostInDB, status_code=status.HTTP_200_OK)
 def get_post(post_id: int, db: Session = Depends(get_db)) -> Post:
-    return fetch_one_post(db=db, post_id=post_id)
+    return fetch_post_from_cache(db=db, post_id=post_id)
 
 
 @router.patch('/{post_id}', response_model=PostInDB, status_code=status.HTTP_201_CREATED)
 def edit_post(post_id: int, obj_in: PostUpdate, db: Session = Depends(get_db)
               , user: UserInDB = Depends(get_current_user)) -> Post:
-    return update_post(db=db, db_obj=get_post(post_id, db), obj_in=obj_in)
+    return update_post(db=db, post_id=post_id, obj_in=obj_in)
 
 
 @router.delete('/{post_id}', response_model=PostInDB, status_code=status.HTTP_200_OK)
 def delete_post(post_id: int, db: Session = Depends(get_db),
-                user: UserInDB = Depends(get_current_user)) -> Post:
-    return remove_post_from_db(db, post_id, user)
+                user: UserInDB = Depends(get_current_user)) -> JSONResponse:
+    return remove_post_from_db(db, post_id)
 
 
 @router.post('/like/{post_id}')
-def add_or_remove_like(post_id: int, db: Session = Depends(get_db), user: UserInDB = Depends(get_current_user)) -> None:
+def add_or_remove_like(post_id: int, db: Session = Depends(get_db),
+                       user: UserInDB = Depends(get_current_user)) -> JSONResponse:
     if check_post_author(db, post_id, user):
-        change_likes_or_dislikes(db, post_id, user, model=Likes)
+        return change_count_of_users_emotions(db=db, user=user, post_id=post_id, table='likes', users='like_user')
     else:
         raise HTTPException(status_code=400, detail="You can't like or dislike your own post")
 
 
 @router.post('/dislike/{post_id}')
 def add_or_remove_dislike(post_id: int, db: Session = Depends(get_db),
-                          user: UserInDB = Depends(get_current_user)) -> None:
+                          user: UserInDB = Depends(get_current_user)) -> JSONResponse:
     if check_post_author(db, post_id, user):
-        change_likes_or_dislikes(db, post_id, user, model=Dislikes)
+        return change_count_of_users_emotions(db=db, user=user, post_id=post_id, table='dislikes', users='dislike_user')
     else:
         raise HTTPException(status_code=400, detail="You can't like or dislike your own post")
---
+++ services/cache.py
@@ -0,0 +1,78 @@
+import datetime
+import logging
+
+import redis as redis
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+from starlette.responses import JSONResponse
+
+from db.crud import fetch_one_post
+from db.models import Likes
+from db.schemas import UserInDB
+
+redis = redis.Redis(host='localhost', port=6379, charset="utf-8", decode_responses=True, db=0)
+
+logger = logging.getLogger('app.services.cache')
+
+
+def fetch_post_from_cache(db: Session, post_id: int):
+    response = redis.hgetall(post_id)
+    if response:
+        return response
+    else:
+        post_from_db = fetch_one_post(db=db, post_id=post_id)
+        if post_from_db:
+            post_from_db['like_user'] = ''
+            post_from_db['dislike_user'] = ''
+            redis.hset(post_from_db['id'], mapping=post_from_db)
+            ttl = datetime.timedelta(hours=720)
+            redis.expire(post_from_db['id'], time=ttl)
+            return post_from_db
+        else:
+            return JSONResponse(status_code=400, content={'msg': 'Post not exist'})
+
+
+def fetch_posts_from_cache(quantity: int, db: Session):
+    posts_for_response = []
+    for i in range(1, quantity + 1):
+        post = fetch_post_from_cache(db=db, post_id=i)
+        if type(post) != dict:
+            continue
+        else:
+            posts_for_response.append(post)
+    return posts_for_response
+
+
+def change_count_of_users_emotions(db: Session, user: UserInDB, post_id: int, table: str, users: str) -> JSONResponse:
+    new_req = redis.hgetall(post_id)
+    if not new_req:
+        single_post = fetch_one_post(db=db, post_id=post_id)
+        if not single_post:
+            raise HTTPException(status_code=400, detail='Post not exists')
+
+        likes_users = db.query(Likes).filter(Likes.post_id == post_id).all()
+        dislikes_users = db.query(Likes).filter(Likes.post_id == post_id).all()
+        dis_users = [str(i.id) for i in dislikes_users]
+        like_users = [str(i.id) for i in likes_users]
+        single_post['dislike_user'] = ':'.join(dis_users)
+        single_post['like_user'] = ':'.join(like_users)
+
+        redis.hset(post_id, mapping=single_post)
+
+        ttl = datetime.timedelta(hours=720)
+        redis.expire(post_id, time=ttl)
+
+    request_after_update = redis.hget(post_id, users)
+    users_list = request_after_update.split(':')
+    if str(user.id) in users_list:
+        users_list.remove(str(user.id))
+        table_data = ':'.join(users_list)
+        redis.hincrby(post_id, table, -1)
+        redis.hset(post_id, users, table_data)
+        return JSONResponse(status_code=201, content={'msg': 'Change your emotion'})
+    else:
+        users_list.append(str(user.id))
+        table_data_users = ':'.join(users_list)
+        redis.hincrby(post_id, table, 1)
+        redis.hset(post_id, users, table_data_users)
+        return JSONResponse(status_code=201, content={'msg': 'Change your emotion'})
